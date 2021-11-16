344e41c8d146cae8574516f5de7cc537948a7109
Your Name <you@example.com>
2021-11-15 21:42:47 -0600

    Uploading files using muler

--- NodeExpress/conFusionServer/authenticate.js
+++ NodeExpress/conFusionServer/authenticate.js
@@ -1,10 +1,11 @@
 var passport = require('passport');
+//passport is for authenticate users
 var LocalStrategy = require('passport-local').Strategy;
+//local is  an strategy for local data
 var User = require('./models/users');
-
-
-
+//here we have the model of users
 var JwtStrategy = require('passport-jwt').Strategy;
+//json web token
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
@@ -24,20 +25,50 @@ var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
-exports.jwtPassport = passport.use(new JwtStrategy(opts,
-    (jwt_payload, done) => {
-        console.log("JWT payload: ", jwt_payload);
-        User.findOne({_id: jwt_payload._id}, (err, user) => {
+exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log("JWT Payload : ", jwt_payload);
+    User.findOne({
+        _id: jwt_payload._id
+    }, (err, user) => {
+        if (err) {
+            return done(err, false);
+        } else if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    })
+
+}));
+
+exports.verifyUser = function (req, res, next) {
+    var token = req.headers.token || req.headers['token'];
+    if (token) {
+        jwt.verify(token, config.secretKey, function (err, decoded) {
             if (err) {
-                return done(err, false);
-            }
-            else if (user) {
-                return done(null, user);
-            }
-            else {
-                return done(null, false);
+                var err = new Error('You are not authenticated!');
+                err.status = 401;
+                return next(err);
+            } else {
+                req.decoded = decoded;
+                next();
             }
         });
-    }));
+    } else {
+        console.log(req.body)
+        var err = new Error('No token provided!');
+        err.status = 403;
+        return next(err);
+    }
+};
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyAdmin = function (req, res, next) {
+    console.log(req.user)
+    if (req) {
+        next();
+    } else {
+        var err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    }
+}
